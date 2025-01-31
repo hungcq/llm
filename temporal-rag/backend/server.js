60c9c93ab91f20ad75d14e17b87c105a91b65341
hungcq <hungcqrt@gmail.com>
2025-01-31 21:11:13 +0700

    switch to new streaming sdk

--- temporal-rag/backend/server.js
+++ temporal-rag/backend/server.js
@@ -1,32 +1,30 @@
-const express = require('express');
-const {Server} = require('socket.io');
-const {OpenAI} = require('openai');
-const dotenv = require('dotenv');
+import express from 'express';
+import {Server} from 'socket.io';
+import OpenAI from 'openai';
+import dotenv from 'dotenv';
+import {Server as HttpServer} from 'http';
 
 // Load environment variables from .env file
 dotenv.config();
 
-// Initialize OpenAI client
-const client = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY, // Make sure to set your OpenAI API key in the .env file
-});
+const openai = new OpenAI();
 
 const app = express();
-const server = require('http').Server(app);
+const server = HttpServer(app);
 const io = new Server(server, {
   cors: {
     origin: '*', // Allow frontend to connect
   },
 });
 
-let assistant = await client.beta.assistants.create({
+let assistant = await openai.beta.assistants.create({
   name: 'hungcq Temporal assistant',
   instructions: 'Use your knowledge base to answer questions about Temporal.',
   model: 'gpt-4o-mini',
   tools: [{type: 'file_search'}],
 });
 
-await client.beta.assistants.update(assistant.id, {
+await openai.beta.assistants.update(assistant.id, {
   tool_resources: {file_search: {vector_store_ids: ['vs_ewunN7laFAOw6m8mEOF5jixY']}},
 });
 
@@ -34,14 +32,14 @@ await client.beta.assistants.update(assistant.id, {
 io.on('connection', async (socket) => {
   console.log('A user connected');
 
-  const thread = await client.beta.threads.create();
+  const thread = await openai.beta.threads.create();
 
   // Listen for 'message' events from the frontend
   socket.on('message', async (msg) => {
     console.log(msg);
     try {
       // Send user message to OpenAI
-      const message = await client.beta.threads.messages.create(
+      const message = await openai.beta.threads.messages.create(
           thread.id,
           {
             role: 'user',
@@ -49,36 +47,38 @@ io.on('connection', async (socket) => {
           },
       );
 
-      const stream = await client.beta.threads.runs.create(
-          thread.id,
-          {assistant_id: assistant.id, stream: true},
-      );
+      const stream = openai.beta.threads.runs.stream(thread.id, {
+        assistant_id: assistant.id,
+      }).
+          on('textCreated', () => console.log('assistant >')).
+          on('toolCallCreated',
+              (event) => console.log('assistant ' + event.type)).
+          on('messageDone', async (event) => {
+            if (event.content[0].type !== 'text') {
+              return;
+            }
+            const {text} = event.content[0];
+            const {annotations} = text;
+            let output = text.value
+            if (!annotations) {
+              socket.emit('message', output);
+              return;
+            }
 
-      for await (const event of stream) {
-        // console.log(JSON.stringify(event))
-        if (event.event !== 'thread.message.delta') {
-          continue;
-        }
-        let text = event.data.delta.content[0].text.value;
-        const annotations = event.data.delta.content[0].text.annotations;
-        if (!annotations) {
-          socket.emit('message', text);
-          continue;
-        }
-        let index = 0;
-        for (let annotation of annotations) {
-          text = text.replace(annotation.text, '[' + index + ']');
-          const {file_citation} = annotation;
-          if (file_citation) {
-            const citedFile = await client.files.retrieve(
-                file_citation.file_id);
-            text = text.replace('[' + index + ']',
-                ' (source: ' + citedFile.filename + ')');
-          }
-          index++;
-        }
-        socket.emit('message', text);
-      }
+            let index = 0;
+            for (let annotation of annotations) {
+              output = output.replace(annotation.text, '[' + index + ']');
+              const {file_citation} = annotation;
+              if (file_citation) {
+                const citedFile = await openai.files.retrieve(
+                    file_citation.file_id);
+                output = output.replace('[' + index + ']',
+                    ' (source: ' + citedFile.filename + ')');
+              }
+              index++;
+            }
+            socket.emit('message', output);
+          });
     } catch (error) {
       console.error('Error:', error);
       socket.emit('message', 'Error occurred');
